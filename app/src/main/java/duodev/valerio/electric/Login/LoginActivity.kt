ba780301fd5bd694041dd247db89f217834ca1b9
Pratyush Udhani <pratyush.udhani@gmail.com>
2020-05-31 22:48:15 +0530

    importing designs and app logo

--- app/src/main/java/duodev/valerio/electric/Login/LoginActivity.kt
+++ app/src/main/java/duodev/valerio/electric/Login/LoginActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.jaeger.library.StatusBarUtil
 import duodev.valerio.electric.R
 import kotlinx.android.synthetic.main.activity_login.*
 
